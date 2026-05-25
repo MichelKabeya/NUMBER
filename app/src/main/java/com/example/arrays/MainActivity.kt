3ca51672d4dabba718dd38a26bbebf7db27db08c
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-05-25 10:54:05 +0200

    declared objects in the ActivityMain

--- app/src/main/java/com/example/arrays/MainActivity.kt
+++ app/src/main/java/com/example/arrays/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.arrays
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val input = findViewById<EditText>(R.id.edtInput)
+        val collect = findViewById<Button>(R.id.addBtn)
+       
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
