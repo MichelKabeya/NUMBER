81920d13ffd3dc4e7621fbbf0c27d1572e158996
MichelKabeya <yssnjz8m2z@privaterelay.appleid.com>
2026-05-25 11:16:51 +0200

    created a function

--- app/src/main/java/com/example/arrays/MainActivity.kt
+++ app/src/main/java/com/example/arrays/MainActivity.kt
@@ -11,15 +11,23 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    private val input = findViewById<EditText>(R.id.edtInput)
-    private val number = IntArray(10)
-    
+    var input = findViewById<EditText>(R.id.edtInput)
+    var index = 0
+    val number = IntArray(10)
+    private fun fillArray(arr: IntArray){
+        for (index in arr.size){
+            IntArray[index] = input.text
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val collect = findViewById<Button>(R.id.addBtn)
+        collect.setOnClickListener {
+            fillArray()
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
